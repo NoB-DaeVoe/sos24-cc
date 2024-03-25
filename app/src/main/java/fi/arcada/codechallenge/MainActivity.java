39c89ceaf4515f887620d3a44d16857dd292f80a
rajalino-arcada <rajalino@arcada.fi>
2024-03-25 15:02:32 +0200

    Add The meaning of mean

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,6 +1,9 @@
 package fi.arcada.codechallenge;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,35 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView outputText;
+    EditText inputText;
+    TextView mean;
+
+    double[] values = { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        outputText = findViewById(R.id.outText);
+        inputText = findViewById(R.id.inText);
+        mean = findViewById(R.id.calc);
+    }
+
+    public void buttonHandler(View view) {
+        String text = inputText.getText().toString();
+        outputText.setText(text);
+    }
+
+    public void calculate(View view) {
+        double sum = 0;
+        for ( int i = 0 ; i < values.length ; i++ ) {
+            sum += values[i];
+        }
+
+        sum /= values.length;
+        mean.setText(String.format("%.2f", sum));
     }
 }
